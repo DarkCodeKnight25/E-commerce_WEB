2a594299a6f6388e31ff75e98a244c3b90c73ba6
DarkCodeKnight25 <darkcodeknight25@gmail.com>
2024-04-08 22:27:56 -0500

    webjava

--- src/main/java/com/example/demo/model/repository/TipoPagoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.repository;
-
-import com.example.demo.model.db.TipoPago;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TipoPagoRepository extends JpaRepository<TipoPago, Integer> {
-}
